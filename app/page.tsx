aa1e4d697a8b0e53b1f08f9889c06be8be02da63
unknown <shcherbakovkirill2@gmail.com>
2024-02-26 20:57:35 +0300

    test three

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const HomePage: FC = () => {
   return (
     
     <main className={s.main}>
-       <Alert severity='success' color='success'>Hell</Alert>
+       <Alert severity='success' color='success'>Helloooo</Alert>
     </main>
   );
 }
